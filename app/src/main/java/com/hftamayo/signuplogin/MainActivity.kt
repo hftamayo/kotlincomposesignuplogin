d5e643914d375f918a7450d9c964a6d526079cf6
hftamayo <hftamayo@gmail.com>
2023-10-09 09:21:44 -0600

    project starting point

---
+++ app/src/main/java/com/hftamayo/signuplogin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.hftamayo.signuplogin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hftamayo.signuplogin.app.PostOfficeApp
+import com.hftamayo.signuplogin.ui.theme.SignupLoginTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PostOfficeApp()
+        }
+    }
+}
---
+++ app/src/main/java/com/hftamayo/signuplogin/app/PostOfficeApp.kt
@@ -0,0 +1,17 @@
+package com.hftamayo.signuplogin.app
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun PostOfficeApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+    ){
+
+    }
+}
