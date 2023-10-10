64e89a64c4967f20ec506e3383de1451e38a58a8
hftamayo <hftamayo@gmail.com>
2023-10-10 10:35:33 -0600

    working on SignUp part1

--- app/src/main/java/com/hftamayo/signuplogin/screens/SignupScreen.kt
+++ app/src/main/java/com/hftamayo/signuplogin/screens/SignupScreen.kt
@@ -1,7 +1,29 @@
 package com.hftamayo.signuplogin.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.HasDefaultViewModelProviderFactory
+import com.hftamayo.signuplogin.R
+import com.hftamayo.signuplogin.components.NormalTextComponent
 
 @Composable
 fun SignupScreen() {
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
+    ){
+        NormalTextComponent(value = stringResource(id = R.string.hello))
+
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreviewOfSignUpScreen(){
+    SignupScreen()
 }
