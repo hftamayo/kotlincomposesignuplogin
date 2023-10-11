489fa6052cac17139ab46c1988d36e2cf4a5aa22
hftamayo <hftamayo@gmail.com>
2023-10-11 10:34:15 -0600

    building signup form part1

--- app/src/main/java/com/hftamayo/signuplogin/components/AppComponents.kt
+++ app/src/main/java/com/hftamayo/signuplogin/components/AppComponents.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hftamayo.signuplogin.R
@@ -26,6 +27,23 @@ fun NormalTextComponent(value:String){
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal,
         )
-    , color = colorResource(id = R.color.colorText)
+    , color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun HeadingTextComponent(value:String){
+    Text(text = value,
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 80.dp),
+        style = TextStyle(
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Normal,
+        )
+        , color = colorResource(id = R.color.colorText),
+        textAlign = TextAlign.Center
     )
 }
--- app/src/main/java/com/hftamayo/signuplogin/screens/SignupScreen.kt
+++ app/src/main/java/com/hftamayo/signuplogin/screens/SignupScreen.kt
@@ -1,24 +1,34 @@
 package com.hftamayo.signuplogin.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.HasDefaultViewModelProviderFactory
 import com.hftamayo.signuplogin.R
+import com.hftamayo.signuplogin.components.HeadingTextComponent
 import com.hftamayo.signuplogin.components.NormalTextComponent
 
 @Composable
 fun SignupScreen() {
     Surface(
         color = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
     ){
-        NormalTextComponent(value = stringResource(id = R.string.hello))
-
+        Column(modifier = Modifier.fillMaxSize()){
+            NormalTextComponent(value = stringResource(id = R.string.hello))
+            HeadingTextComponent(value = stringResource(id = R.string.create_account))
+        }
     }
 }
 
