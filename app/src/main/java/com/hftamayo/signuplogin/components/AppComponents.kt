3e810a0d945d8e2d7b066bfd36c4fd82f4c3e0c6
hftamayo <hftamayo@gmail.com>
2023-10-11 10:17:34 -0600

    using color palette

--- app/src/main/java/com/hftamayo/signuplogin/components/AppComponents.kt
+++ app/src/main/java/com/hftamayo/signuplogin/components/AppComponents.kt
@@ -5,22 +5,27 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hftamayo.signuplogin.R
+import com.hftamayo.signuplogin.ui.theme.TextColor
 
 
 @Composable
 fun NormalTextComponent(value:String){
     Text(text = value,
-    modifier = Modifier.fillMaxWidth().heightIn(min = 80.dp),
+    modifier = Modifier
+        .fillMaxWidth()
+        .heightIn(min = 80.dp),
         style = TextStyle(
             fontSize = 18.sp,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal,
         )
+    , color = colorResource(id = R.color.colorText)
     )
 }
