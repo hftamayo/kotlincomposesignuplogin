5816e193712b5e6002ec9c8db42bfbe6dc99964b
hftamayo <hftamayo@gmail.com>
2023-10-20 10:53:56 -0600

    asembling app with termas and conditions screen

--- app/src/main/java/com/hftamayo/signuplogin/app/PostOfficeApp.kt
+++ app/src/main/java/com/hftamayo/signuplogin/app/PostOfficeApp.kt
@@ -1,11 +1,16 @@
 package com.hftamayo.signuplogin.app
 
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.hftamayo.signuplogin.navigation.PostOfficeAppRouter
+import com.hftamayo.signuplogin.navigation.Screen
+import com.hftamayo.signuplogin.screens.SignupScreen
+import com.hftamayo.signuplogin.screens.TermAndConditScreen
 
 @Composable
 fun PostOfficeApp(){
@@ -13,6 +18,15 @@ fun PostOfficeApp(){
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ){
-
+        Crossfade(targetState = PostOfficeAppRouter.currentScreen) { currentState->
+            when(currentState.value){
+                is Screen.SignUpScreen -> {
+                    SignupScreen()
+                }
+                is Screen.TermAndConditScreen -> {
+                    TermAndConditScreen()
+                }
+            }
+        }
     }
 }
