c6342d7716a0af486a628d3209e192cd81934c4b
hftamayo <hftamayo@gmail.com>
2023-10-20 10:51:37 -0600

    updating app's components

--- app/src/main/java/com/hftamayo/signuplogin/components/AppComponents.kt
+++ app/src/main/java/com/hftamayo/signuplogin/components/AppComponents.kt
@@ -1,10 +1,14 @@
 package com.hftamayo.signuplogin.components
 
+import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -13,19 +17,23 @@ import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hftamayo.signuplogin.R
@@ -138,4 +146,51 @@ fun PasswordTextFieldComponent(labelValue: String, painterResource: Painter){
         },
         visualTransformation = if(passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation ()
     )
+}
+
+@Composable
+fun CheckBoxComponent(value: String, onTextSelected: (String) -> Unit){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .heightIn(56.dp)
+        .padding(16.dp), verticalAlignment = Alignment.CenterVertically,){
+        val checkedState = remember {
+            mutableStateOf(false)
+        }
+        Checkbox(checked = checkedState.value, onCheckedChange = {
+            checkedState.value = !checkedState.value
+        })
+        ClickableTextComponent(value = value,onTextSelected)
+    }
+}
+
+@Composable
+fun ClickableTextComponent(value: String, onTextSelected : (String) -> Unit){
+    val initialText = "By continuing you accept our ..."
+    val privacyPolicyText = "Privacy Policy "
+    val andText = " and "
+    val termsAndConditionsText = " Terms of Use"
+
+    val annotatedString = buildAnnotatedString {
+        append(initialText)
+        withStyle(style = SpanStyle(color = Primary)){
+            pushStringAnnotation(tag = privacyPolicyText, annotation = privacyPolicyText)
+            append(privacyPolicyText)
+        }
+        append(andText)
+        withStyle(style = SpanStyle(color = Primary)){
+            pushStringAnnotation(tag = termsAndConditionsText, annotation = termsAndConditionsText)
+            append(termsAndConditionsText)
+        }
+    }
+    ClickableText(text = annotatedString, onClick ={offset ->
+        annotatedString.getStringAnnotations(offset, offset)
+            .firstOrNull()?.also {span ->
+                Log.d("ClickableTextComponent","{${span.item}}")
+                if(span.item == termsAndConditionsText){
+                    onTextSelected(span.item)
+                }
+            }
+
+    } )
 }
